152447b9e05180295cfc0a2a1a8be6c8b4edfd8b
amit <amit@techprimelab.com>
2017-08-25 16:50:31 +0530

    added new db connection and created global variable which is connection

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-
+var dbvar;
 // Connect to the db
 /*MongoClient.connect("mongodb://localhost:27017/mongo1", function (err, db) {
 
@@ -28,37 +28,19 @@ var MongoClient = require('mongodb').MongoClient;
 
 module.exports = {
      connect: function(html) {
-        MongoClient.connect("mongodb://localhost:27017/mongo1", function (err, db) {
-
-            db.collection('Person1', function (err, collection) {
-
-                collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-                collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-                collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
-
-
-
-                db.collection('Persons11').count(function (err, count) {
-                    if (err) throw err;
-
-                    console.log('Total Rows: ' + count);
-                });
-            });
-
+        MongoClient.connect("mongodb://localhost:27017/uses-tracking", function (err, db) {
+            dbvar =db;
         });
     },
     insertIntoDB: function(){
-        db.collection('Person1', function (err, collection) {
+        dbvar.collection('', function (err, collection) {
 
-            collection.insert({ id: 11, firstName: 'Steve1', lastName: 'Jobs1' });
-            collection.insert({ id: 21, firstName: 'Bill1', lastName: 'Gates1' });
-            collection.insert({ id: 31, firstName: 'James1', lastName: 'Bond1' });
+            collection.insert({ id: 1, firstName: 'Amit', lastName: 'Gaikwad' });
+            collection.insert({ id: 2, firstName: 'Bill1', lastName: 'Gates1' });
+            collection.insert({ id: 3, firstName: 'James1', lastName: 'Bond1' });
 
-
-
-            db.collection('Person12').count(function (err, count) {
+            dbvar.collection('authors').count(function (err, count) {
                 if (err) throw err;
-
                 console.log('Total Rows: ' + count);
             });
         });
