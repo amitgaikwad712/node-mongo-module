9a38146c0fd9883a3d3451012349fa62d84b7d76
amit <amit@techprimelab.com>
2017-08-28 10:53:38 +0530

    added mongoose support into module

--- app.js
+++ app.js
@@ -1,49 +1,22 @@
 var MongoClient = require('mongodb').MongoClient;
+var Mongoose = require("mongoose");
+var mongooseInstance;
+var mongooseConnection;
 var dbvar;
-// Connect to the db
-/*MongoClient.connect("mongodb://localhost:27017/mongo1", function (err, db) {
-
-    db.collection('Person1', function (err, collection) {
-
-        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
-
-
-
-        db.collection('Persons').count(function (err, count) {
-            if (err) throw err;
-
-            console.log('Total Rows: ' + count);
-        });
-    });
-
-});*/
-
-
-/*module.exports = function () {
-    console.log('bar!');
-}*/
-
 
 module.exports = {
-     connect: function(html) {
-        MongoClient.connect("mongodb://localhost:27017/uses-tracking", function (err, db) {
-            dbvar =db;
-        });
+     connect: function() {
+         mongooseConnection = Mongoose.connection;
+         mongooseConnection.once('open', () => {
+             console.log('Connected to its me.');
+         });
+         mongooseInstance = Mongoose.connect("mongodb://localhost:27017/uses-tracking");
+
+
     },
     insertIntoDB: function(){
-        dbvar.collection('', function (err, collection) {
+            console.log('mongoose mongooseInstance '+mongooseInstance);
+            console.log('mongooseConnection data'+mongooseConnection);
+    },
 
-            collection.insert({ id: 1, firstName: 'Amit', lastName: 'Gaikwad' });
-            collection.insert({ id: 2, firstName: 'Bill1', lastName: 'Gates1' });
-            collection.insert({ id: 3, firstName: 'James1', lastName: 'Bond1' });
-
-            dbvar.collection('authors').count(function (err, count) {
-                if (err) throw err;
-                console.log('Total Rows: ' + count);
-            });
-        });
-
-    }
 };
